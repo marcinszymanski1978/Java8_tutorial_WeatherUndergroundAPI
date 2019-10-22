6695e9e7cf9cdba349c65704458375eafe8c87c5
msz1978 <msz1978@wp.pl>
2019-10-22 09:42:52 +0200

    Tests

---
+++ src/test/java/CurrentWeatherTest.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class CurrentWeatherTest extends TestCase {
+@Test
+    public void testConnect() {
+        
+    }
+
+    public void testPrintCurrentWeatherMap() {
+    }
+
+    public void testGetTemp() {
+    }
+}
---
+++ src/test/java/Http_Config_DatasTest.java
@@ -0,0 +1,9 @@
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+public class Http_Config_DatasTest extends TestCase {
+
+    public void testGetLinkConnection_WU_URL() {
+
+    }
+}
